c8995fd9ffa81293fa977bdd475eb35b35d6406a
pixelplex-dev <dev@pixelplex.by>
2020-12-03 16:23:46 +0300

    [FIL-9] Create command-line interface stub

---
+++ cli/contract.go
@@ -0,0 +1,102 @@
+package cli
+
+import (
+	"github.com/urfave/cli/v2"
+)
+
+var contractCmd = &cli.Command{
+	Name:  "contract",
+	Usage: "Manage smart contracts",
+	Subcommands: []*cli.Command{
+		contractCreate,
+		contractCall,
+	},
+}
+
+var contractCreate = &cli.Command{
+	Name:      "create",
+	Usage:     "create smart contract",
+	ArgsUsage: "[amount] [contract-code]",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "from",
+			Usage: "optionally specify the account to send funds from",
+		},
+		&cli.StringFlag{
+			Name:  "gas-premium",
+			Usage: "specify gas price to use in AttoFIL",
+			Value: "0",
+		},
+		&cli.StringFlag{
+			Name:  "gas-feecap",
+			Usage: "specify gas fee cap to use in AttoFIL",
+			Value: "0",
+		},
+		&cli.Int64Flag{
+			Name:  "gas-limit",
+			Usage: "specify gas limit",
+			Value: 0,
+		},
+		&cli.Int64Flag{
+			Name:  "nonce",
+			Usage: "specify the nonce to use",
+			Value: -1,
+		},
+	},
+	Action: func(cctx *cli.Context) error {
+		/*
+			api, closer, err := GetFullNodeAPI(cctx)
+			if err != nil {
+				return err
+			}
+			defer closer()
+			ctx := ReqContext(cctx)
+		*/
+
+		return nil
+	},
+}
+
+var contractCall = &cli.Command{
+	Name:      "call",
+	Usage:     "Call smart contract method",
+	ArgsUsage: "[address] [amount] [call-code]",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "from",
+			Usage: "optionally specify the account to send funds from",
+		},
+		&cli.StringFlag{
+			Name:  "gas-premium",
+			Usage: "specify gas price to use in AttoFIL",
+			Value: "0",
+		},
+		&cli.StringFlag{
+			Name:  "gas-feecap",
+			Usage: "specify gas fee cap to use in AttoFIL",
+			Value: "0",
+		},
+		&cli.Int64Flag{
+			Name:  "gas-limit",
+			Usage: "specify gas limit",
+			Value: 0,
+		},
+		&cli.Int64Flag{
+			Name:  "nonce",
+			Usage: "specify the nonce to use",
+			Value: -1,
+		},
+	},
+	Action: func(cctx *cli.Context) error {
+		/*
+			api, closer, err := GetFullNodeAPI(cctx)
+			if err != nil {
+				return err
+			}
+			defer closer()
+			ctx := ReqContext(cctx)
+		*/
+
+		return nil
+	},
+}
